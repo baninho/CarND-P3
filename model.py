15339a82eb400158fbe587f827ecdf7049a148b4
baninho <florianimbt@gmail.com>
2017-05-26 16:20:01 +0200

    use generator for training

--- model.py
+++ model.py
@@ -2,35 +2,44 @@ import csv
 import cv2
 import numpy as np
 
+from sklearn.model_selection import train_test_split
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda
 from keras.layers.convolutional import Convolution2D, Cropping2D
 from keras.layers.pooling import MaxPooling2D
 
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
+        shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+
+            images = []
+            angles = []
+            for batch_sample in batch_samples:
+                name = './IMG/'+batch_sample[0].split('/')[-1]
+                center_image = cv2.imread(name)
+                center_angle = float(batch_sample[3])
+                images.append(center_image)
+                angles.append(center_angle)
+
+            # trim image to only see section with road
+            X_train = np.array(images)
+            y_train = np.array(angles)
+            yield sklearn.utils.shuffle(X_train, y_train)
+
+# read csv
 lines = []
 with open('./data/driving_log.csv') as f:
 	reader = csv.reader(f)
 	for line in reader:
 		lines.append(line)
 
-images = []
-measurements = []
+train_samples, validation_samples = train_test_split(lines, test_size=0.2)
 
-for line in lines:
-	if 'center' == line[0]:
-		continue
-	source_path = line[0]
-	filename = source_path.split('\\')[-1]
-	current_path = './data/IMG/' + filename
-	
-	image = cv2.imread(current_path)
-	images.append(image)
-
-	measurement = float(line[3])
-	measurements.append(measurement)
-
-X_train = np.array(images)
-y_train = np.array(measurements)
+train_generator = generator(train_samples, batch_size=32)
+validation_generator = generator(validation_samples, batch_size=32)
 
 model = Sequential()
 model.add(Lambda(lambda x: x / 127.5 - 1.0, input_shape = (160,320,3)))
@@ -47,6 +56,6 @@ model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss = 'mse', optimizer = 'adam')
-model.fit(X_train, y_train, nb_epoch = 5, validation_split = 0.2, shuffle = True)
+model.fit_generator(train_generator, samples_per_epoch = len(train_samples), validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=5)
 
 model.save('model.h5')
